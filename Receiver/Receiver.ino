6b23509e42707b9a78133bf0915f378c132e6dad
Yoseph Alabdulwahab <yosephsa0@gmail.com>
2016-02-21 13:32:02 -0800

    The initial code setup. Will only read if laser is pointed.

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,9 +1,17 @@
+#define PHOTO_PIN 11
+#define LED_PIN 13
+
+
 void setup()
 {
   Serial.begin(9600);
+  pinMode(PHOTO_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-  
+  Serial.println(digitalRead(PHOTO_PIN));
+  int photo = digitalRead(PHOTO_PIN);
+  digitalWrite(LED_PIN, photo);
 }
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,9 +1,12 @@
+#define LED_PIN 11
+
 void setup()
 {
   Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-  
+  digitalWrite(LED_PIN, HIGH);
 }
