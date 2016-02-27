92784cd79ef8ad27a9926294a5f7b91ae9848ef8
Yoseph Alabdulwahab <yosephsa0@gmail.com>
2016-02-26 16:00:42 -0800

    Prototyped Sound Writer

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,17 +1,31 @@
 #define PHOTO_PIN 11
 #define LED_PIN 13
-
+#define PIN_SPEAKER = 10;
 
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(PHOTO_PIN, INPUT);
-  pinMode(LED_PIN, OUTPUT);
+  
+}
+
+void do_sound(byte in)
+{
+  for(int i = 0; i < 8; i++)
+  {
+    if((0x80 >> i) | in)
+    {
+      digitalWrite(SPEAKER_PIN,HIGH);
+    }
+  else  
+  {
+    digitalWrite(SPEAKER_PIN, LOW);
+  }  
 }
 
 void loop()
 {
-  Serial.println(digitalRead(PHOTO_PIN));
-  int photo = digitalRead(PHOTO_PIN);
-  digitalWrite(LED_PIN, photo);
+  byte dat;
+  if(Serial.available())
+    dat = Serial.read();
+   op_sound(dat);
+   
 }
