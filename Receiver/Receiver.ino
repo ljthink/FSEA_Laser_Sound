d9046daf65340eaa52aad6f4d1769ffdd7aada1e
Jack <emububbles1@gmail.com>
2016-03-02 11:59:32 -0800

    first revision. Added synchronization and simple PWM-like sound. Configured for 10472 Bps.

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,17 +1,46 @@
 #define PHOTO_PIN 11
 #define LED_PIN 13
-
+#define SPEAKER_PIN 15
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200,SERIAL_8E2);                //begin serial with 115200 baud, even parity bit, and two end bits.
   pinMode(PHOTO_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
+  for(int i = 0; i < 5000; i++)
+    {
+      if(Serial.available() >= 2)
+        {
+          int time = 0;
+          Serial.readBytes(&time,4);
+          Serial.println(time-i);
+          i = time;
+        }
+      delay(1);
+    }
+}
+
+unsigned int delaytimes[2];
+
+void PCM_to_delaytimes(unsigned char c)
+{
+  delaytimes[0] = ((255 - c)/510.0)*95;
+  delaytimes[1] = (c/255.0)*95;
+}
+
+void mk_sound()
+{
+  delayMicroseconds(delaytimes[0]);
+  digitalWrite(SPEAKER_PIN,HIGH);
+  delayMicroseconds(delaytimes[1]);
+  digitalWrite(SPEAKER_PIN,LOW);
 }
 
 void loop()
 {
-  Serial.println(digitalRead(PHOTO_PIN));
-  int photo = digitalRead(PHOTO_PIN);
-  digitalWrite(LED_PIN, photo);
+  if(Serial.available())
+    {
+      PCM_to_delaytimes(Serial.read());
+      mk_sound();
+    }
 }
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,12 +1,26 @@
 #define LED_PIN 11
 
+unsigned char int_to_char(int i)
+{
+  return (unsigned char) (i & 255);
+}
+
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200,SERIAL_8E2);         //begin serial with 115200 baud, even parity bit, and two end bits.
+                                           //115200 - 115200*(3/11) = 83781 bps = 10472 Bps
   pinMode(LED_PIN, OUTPUT);
+  for(int i = 0; i < 5000; i ++)           //synchronize arduinos
+    {
+      Serial.write(&i,4);
+      delay(1);
+    }
 }
 
 void loop()
 {
-  digitalWrite(LED_PIN, HIGH);
+  if(Serial.available())
+    {
+      Serial.write(int_to_char(Serial.read()));
+    }
 }
