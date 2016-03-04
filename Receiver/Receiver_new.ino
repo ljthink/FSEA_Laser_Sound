809edc4cfeb991548388ec5d4f9189b2c19ac007
Jack <emububbles1@gmail.com>
2016-03-04 15:35:12 -0800

    fix some crap

---
+++ Receiver/Receiver_new.ino
@@ -0,0 +1,71 @@
+#define PHOTO_PIN 11
+#define LED_PIN 13
+#define SPEAKER_PIN 15
+
+void setup()
+{
+  Serial.begin(115200,SERIAL_8E2);                //begin serial with 115200 baud, even parity bit, and two end bits.
+  pinMode(PHOTO_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  for(int i = 0; i < 5000; i++)
+    {
+      if(Serial.available() >= 2)
+        {
+          int time = 0;
+          Serial.readBytes(&time,4);
+          Serial.println(time-i);
+          i = time;
+        }
+      delay(1);
+    }
+}
+
+unsigned int delaytimes[2];
+
+void PCM_to_delaytimes(unsigned char c)
+{
+  delaytimes[0] = ((255 - c)/510.0)*95;
+  delaytimes[1] = (c/255.0)*95;
+}
+
+void mk_sound()
+{
+  delayMicroseconds(delaytimes[0]);
+  digitalWrite(SPEAKER_PIN,HIGH);
+  delayMicroseconds(delaytimes[1]);
+  digitalWrite(SPEAKER_PIN,LOW);
+=======
+#define PIN_SPEAKER = 10;
+
+void setup()
+{
+  
+}
+
+void do_sound(byte in)
+{
+  for(int i = 0; i < 8; i++)
+  {
+    if((0x80 >> i) | in)
+    {
+      digitalWrite(SPEAKER_PIN,HIGH);
+    }
+  else  
+  {
+    digitalWrite(SPEAKER_PIN, LOW);
+  }  
+}
+
+void loop()
+{
+  if(Serial.available())
+    {
+      PCM_to_delaytimes(Serial.read());
+      mk_sound();
+    }
+  byte dat;
+  if(Serial.available())
+    dat = Serial.read();
+   op_sound(dat);
+   
+}
